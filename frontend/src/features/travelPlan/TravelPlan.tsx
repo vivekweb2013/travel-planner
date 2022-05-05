50516c9b44811cc49dea4874fcb4fee6375c8d3f
vivekweb2013 <vivekweb2013@gmail.com>
2022-05-05 23:59:16 +0530

    Add autocomplete input for city selection

--- frontend/src/features/travelPlan/TravelPlan.tsx
+++ frontend/src/features/travelPlan/TravelPlan.tsx
@@ -1,4 +1,4 @@
-import { Autocomplete, FormControl, InputLabel, TextField } from '@mui/material';
+import { Autocomplete, FormControl, TextField } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import {
@@ -12,17 +12,21 @@ export function TravelPlan() {
   const dispatch = useAppDispatch();
   const [city, setCity] = React.useState<City | null>();
 
+  const handleQueryChange = (newValue: City | null) => {
+    setCity(newValue);
+  }
   useEffect(() => {
-    dispatch(fetchCitiesAsync(""));
-  }, [])
+    dispatch(fetchCitiesAsync("New"));
+  }, [dispatch])
 
   return (
     <div>
       <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-name-label">Name</InputLabel>
         <Autocomplete id="city-select" options={cities}
+          getOptionLabel={(option) => option.name}
+          renderOption={(props, option) => <li {...props}>{option.name}</li>}
           renderInput={(params) => <TextField {...params} label="Select City" />} value={city}
-          onChange={(event: any, newValue: City | null) => { setCity(newValue); }}
+          onChange={(event: any, newValue: City | null) => { handleQueryChange(newValue); }}
         />
       </FormControl>
     </div>
